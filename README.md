719a52d7350f5cae748ca304cab5b39fb634bab7
Fedir <99473719+cyanidium1@users.noreply.github.com>
2023-09-03 00:46:11 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Novaposhta tracking app.
--- src/pages/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-export const NotFound = () => {
-  return (
-    <main style={{ textAlign: "center" }}>
-      <b style={{ fontSize: 64 }}>404</b>
-      <p>Сторінку не знайдено :(</p>
-    </main>
-  );
-};
