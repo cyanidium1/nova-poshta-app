42f5ef819506a81ad85440ce53334ab5a6f88f31
Fedir <99473719+cyanidium1@users.noreply.github.com>
2023-09-03 00:51:55 +0200

    I HATE GHPAGES!

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import App from "./components/App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/nova-poshta-app">
+    <BrowserRouter basename="/nova-poshta-app">
       <App />
-    </BrowserRouter> */}
-    <div>чертов гітхаб пейджс ска</div>
+    </BrowserRouter>
+    {/* <div>чертов гітхаб пейджс ска</div> */}
   </React.StrictMode>
 );
